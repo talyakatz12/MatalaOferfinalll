b44a6ecc424b7bf7b58bcbc57bce9bd8931acb75
Talya Katz <talyakatz12@gmail.com>
2026-02-19 20:53:28 +0200

    current broken version

--- src/pages/EmployeePage.jsx
+++ src/pages/EmployeePage.jsx
@@ -3,6 +3,19 @@ import { useSearchParams, useNavigate } from 'react-router-dom';
 import { EmployeesContext } from '../context/EmployeesContext';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const DefaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 
 export default function EmployeePage() {
   useEffect(() => {
